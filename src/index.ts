4654b12ce19f63689fdb55f505567963783edd5e
Kaiser <kaiser@sapling.gg>
2022-03-21 11:20:01 -0700

    1.0.12

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 // Uses decentraland-ecs for types
 
+function track() {
+  // Track a user's position, TODO: Report this to endpoint
+  setInterval(() => {
+    log(Camera.instance.feetPosition);
+    log(Camera.instance.rotation.eulerAngles);
+  }, 1000);
+}
+
 export function CreateBillboard(
   engine: Engine,
   location = { x: 0, y: 0, z: 0 },
@@ -9,18 +17,25 @@ export function CreateBillboard(
   clickUrl = 'https://www.sapling.gg',
 ) {
   const depth = 0.05;
+
   const wig = 0.05;
 
   const { width, height } = scale;
+
   const { x, y, z } = location;
+
   const { r1, r2, r3, r4 } = rotation;
+
   const raise = height / 2 + 1;
 
   const faceTexture = new Texture(imageUrl);
+
   const faceMaterial = new Material();
+
   faceMaterial.albedoTexture = faceTexture;
 
   const front = new Entity();
+
   front.addComponent(new BoxShape());
 
   // Create entity and assign shape
@@ -31,12 +46,17 @@ export function CreateBillboard(
       scale: new Vector3(width, height, depth),
     }),
   );
+
   front.addComponentOrReplace(faceMaterial);
+
   front.addComponent(new OnPointerDown(() => openExternalURL(clickUrl)));
+
   engine.addEntity(front);
 
   const back = new Entity();
+
   back.addComponent(new BoxShape());
+
   // Create entity and assign shape
   back.addComponentOrReplace(
     new Transform({
@@ -45,12 +65,17 @@ export function CreateBillboard(
       scale: new Vector3(width, height, depth),
     }),
   );
+
   back.addComponentOrReplace(faceMaterial);
+
   back.addComponent(new OnPointerDown(() => openExternalURL(clickUrl)));
+
   engine.addEntity(back);
 
   const postL = new Entity();
+
   postL.addComponent(new BoxShape());
+
   // Create entity and assign shape
   postL.addComponentOrReplace(
     new Transform({
@@ -59,10 +84,13 @@ export function CreateBillboard(
       scale: new Vector3(0.1, height + 2 * raise, depth * 2),
     }),
   );
+
   engine.addEntity(postL);
 
   const postR = new Entity();
+
   postR.addComponent(new BoxShape());
+
   // Create entity and assign shape
   postR.addComponentOrReplace(
     new Transform({
@@ -71,5 +99,6 @@ export function CreateBillboard(
       scale: new Vector3(0.1, height + 2 * raise, depth * 2),
     }),
   );
+
   engine.addEntity(postR);
 }
