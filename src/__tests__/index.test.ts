c7314f6448188b6e9295ab8218fd7a439c17c4d9
Kaiser <kaiser@sapling.gg>
2022-03-21 11:43:01 -0700

    1.0.13

--- src/__tests__/index.test.ts
+++
@@ -1,7 +0,0 @@
-import 'jest';
-import { CreateBillboard } from '../index';
-
-test('Creating a billboard', () => {
-  const engine = undefined;
-  // expect(CreateBillboard(engine)).toBe(undefined);
-});
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@
 
 function track() {
   // Track a user's position, TODO: Report this to endpoint
+  /* setInterval needs to be defined
   setInterval(() => {
     log(Camera.instance.feetPosition);
     log(Camera.instance.rotation.eulerAngles);
   }, 1000);
+  */
 }
 
 export function CreateBillboard(
@@ -17,28 +19,19 @@ export function CreateBillboard(
   clickUrl = 'https://www.sapling.gg',
 ) {
   const depth = 0.05;
-
   const wig = 0.05;
 
   const { width, height } = scale;
-
   const { x, y, z } = location;
-
   const { r1, r2, r3, r4 } = rotation;
-
   const raise = height / 2 + 1;
 
   const faceTexture = new Texture(imageUrl);
-
   const faceMaterial = new Material();
-
   faceMaterial.albedoTexture = faceTexture;
 
   const front = new Entity();
-
   front.addComponent(new BoxShape());
-
-  // Create entity and assign shape
   front.addComponentOrReplace(
     new Transform({
       position: new Vector3(x, y + raise, z - depth / 2),
@@ -48,16 +41,11 @@ export function CreateBillboard(
   );
 
   front.addComponentOrReplace(faceMaterial);
-
   front.addComponent(new OnPointerDown(() => openExternalURL(clickUrl)));
-
   engine.addEntity(front);
 
   const back = new Entity();
-
   back.addComponent(new BoxShape());
-
-  // Create entity and assign shape
   back.addComponentOrReplace(
     new Transform({
       position: new Vector3(x, y + raise, z + depth / 2),
@@ -65,18 +53,12 @@ export function CreateBillboard(
       scale: new Vector3(width, height, depth),
     }),
   );
-
   back.addComponentOrReplace(faceMaterial);
-
   back.addComponent(new OnPointerDown(() => openExternalURL(clickUrl)));
-
   engine.addEntity(back);
 
   const postL = new Entity();
-
   postL.addComponent(new BoxShape());
-
-  // Create entity and assign shape
   postL.addComponentOrReplace(
     new Transform({
       position: new Vector3(x - width / 2 - wig, y, z),
@@ -84,14 +66,10 @@ export function CreateBillboard(
       scale: new Vector3(0.1, height + 2 * raise, depth * 2),
     }),
   );
-
   engine.addEntity(postL);
 
   const postR = new Entity();
-
   postR.addComponent(new BoxShape());
-
-  // Create entity and assign shape
   postR.addComponentOrReplace(
     new Transform({
       position: new Vector3(x + width / 2 + wig, y, z),
@@ -99,6 +77,5 @@ export function CreateBillboard(
       scale: new Vector3(0.1, height + 2 * raise, depth * 2),
     }),
   );
-
   engine.addEntity(postR);
 }
